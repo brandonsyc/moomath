520dee8d1502e067d169f50d44908ba511048db5
Nichodon <brandonsyc.2222@yahoo.com>
2017-05-20 09:22:39 -0700

    fix article navbar

--- fractals/navbar.js
+++ fractals/navbar.js
@@ -1,11 +1,11 @@
 document.getElementsByClassName("topnav")[0].innerHTML =
-	'<a href=..\"index.html\">Home</a>' +
-	'<a href=..\"fractals.html\">Fractals</a>' +
-	'<a href=..\"coming_soon\">Programming</a>' +
-	'<a href=..\"coming_soon\">Calculators</a>' +
-	'<a href=..\"coming_soon\">More Math</a>' +
-	'<a href=..\"coming_soon\">Worksheets</a>' +
-	'<a href=..\"about.html\">About</a>';
+	'<a href=\"../index.html\">Home</a>' +
+	'<a href=\"../fractals.html\">Fractals</a>' +
+	'<a href=\"../coming_soon\">Programming</a>' +
+	'<a href=\"../coming_soon\">Calculators</a>' +
+	'<a href=\"../coming_soon\">More Math</a>' +
+	'<a href=\"../coming_soon\">Worksheets</a>' +
+	'<a href=\"../about.html\">About</a>';
 
 var linkz = document.getElementsByClassName("topnav")[0].children;
 for (var i = 0; i < linkz.length; i++) {
