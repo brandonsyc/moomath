ae1185b389918d5c6dc52b8a3066121bd4402b99
Nichodon <brandonsyc.2222@yahoo.com>
2017-05-20 09:50:57 -0700

    add footer js

--- navbar.js
+++ navbar.js
@@ -1,11 +1,17 @@
-document.getElementsByClassName("topnav")[0].innerHTML =
-	'<a href=\"https://nichodon.github.io\">Home</a>' +
-	'<a href=\"fractals.html\">Fractals</a>' +
-	'<a href=\"coming_soon\">Programming</a>' +
-	'<a href=\"coming_soon\">Calculators</a>' +
-	'<a href=\"coming_soon\">More Math</a>' +
-	'<a href=\"coming_soon\">Worksheets</a>' +
-	'<a href=\"about.html\">About</a>';
+document.getElementById("top").innerHTML =
+	'<div class="content">' +
+		'<h1>VISUAL MATH</h1>' +
+	'</div>' +
+
+	'<div class="topnav">' +
+		'<a href=\"https://nichodon.github.io\">Home</a>' +
+		'<a href=\"fractals.html\">Fractals</a>' +
+		'<a href=\"coming_soon\">Programming</a>' +
+		'<a href=\"coming_soon\">Calculators</a>' +
+		'<a href=\"coming_soon\">More Math</a>' +
+		'<a href=\"coming_soon\">Worksheets</a>' +
+		'<a href=\"about.html\">About</a>' +
+	'</div>';
 
 var linkz = document.getElementsByClassName("topnav")[0].children;
 for (var i = 0; i < linkz.length; i++) {
