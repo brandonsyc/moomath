91d10ad3b7c08b17a2d80f38a7dbd52bf5fb2f26
Nichodon <brandonsyc.2222@yahoo.com>
2017-05-21 06:27:59 -0700

    remove .html in url
    subdirectories

--- navbar.js
+++ navbar.js
@@ -5,17 +5,16 @@ document.getElementById("top").innerHTML =
 
 	'<div class="topnav">' +
 		'<a href=\"https://nichodon.github.io\">Home</a>' +
-		'<a href=\"../fractals/\">Fractals</a>' +
-		'<a href=\"../programming.html\">Programming</a>' +
-		'<a href=\"../coming_soon\">Calculators</a>' +
-		'<a href=\"../coming_soon\">More Math</a>' +
-		'<a href=\"../coming_soon\">Worksheets</a>' +
-		'<a href=\"../about.html\">About</a>' +
+		'<a href=\"https://nichodon.github.io/fractals/\">Fractals</a>' +
+		'<a href=\"https://nichodon.github.io/programming/\">Programming</a>' +
+		'<a href=\"https://nichodon.github.io/coming_soon/\">Calculators</a>' +
+		'<a href=\"https://nichodon.github.io/coming_soon/\">More Math</a>' +
+		'<a href=\"https://nichodon.github.io/coming_soon/\">Worksheets</a>' +
+		'<a href=\"https://nichodon.github.io/about.html\">About</a>' +
 	'</div>';
 
 var linkz = document.getElementsByClassName("topnav")[0].children;
 for (var i = 0; i < linkz.length; i++) {
-	alert(linkz[i].href);
 	if (linkz[i].href === window.location.href) {
 		linkz[i].classList.add('active');
 	}
