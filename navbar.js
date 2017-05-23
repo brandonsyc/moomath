4b338b8028b0b5c8816fe6fcdf2591deffc77adf
Nichodon <brandonsyc.2222@yahoo.com>
2017-05-22 19:32:19 -0700

    change theme
    dark to light nav and footer

--- navbar.js
+++ navbar.js
@@ -4,13 +4,13 @@ document.getElementById("top").innerHTML =
 	'</div>' +
 
 	'<div class="topnav">' +
-		'<a href=\"https://nichodon.github.io\">Home</a>' +
-		'<a href=\"https://nichodon.github.io/fractals/\">Fractals</a>' +
-		'<a href=\"https://nichodon.github.io/programming/\">Programming</a>' +
-		'<a href=\"https://nichodon.github.io/coming_soon/\">Calculators</a>' +
-		'<a href=\"https://nichodon.github.io/coming_soon/\">More Math</a>' +
-		'<a href=\"https://nichodon.github.io/coming_soon/\">Worksheets</a>' +
-		'<a href=\"https://nichodon.github.io/about/\">About</a>' +
+		'<a href="https://nichodon.github.io">Home</a>' +
+		'<a href="https://nichodon.github.io/fractals/">Fractals</a>' +
+		'<a href="https://nichodon.github.io/programming/">Programming</a>' +
+		'<a href="https://nichodon.github.io/coming_soon/">Calculators</a>' +
+		'<a href="https://nichodon.github.io/coming_soon/">More Math</a>' +
+		'<a href="https://nichodon.github.io/coming_soon/">Worksheets</a>' +
+		'<a href="https://nichodon.github.io/about/">About</a>' +
 	'</div>';
 
 var linkz = document.getElementsByClassName("topnav")[0].children;
