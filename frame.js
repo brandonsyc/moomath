60932f446891a2e870c8e187bb5848ec626292de
Nichodon <brandonsyc.2222@yahoo.com>
2017-05-25 06:54:21 -0700

    make sticky smoother

--- frame.js
+++ frame.js
@@ -12,8 +12,18 @@ document.getElementById("top").innerHTML =
 		'<a href="https://nichodon.github.io/coming_soon/">More Math</a>' +
 		'<a href="https://nichodon.github.io/coming_soon/">Worksheets</a>' +
 		'<a href="https://nichodon.github.io/about/">About</a>' +
-	'</div>';
+	'</div>' +
 
+
+	'<div class="topnav" id="sticky">' +
+		'<a href="https://nichodon.github.io">Home</a>' +
+		'<a href="https://nichodon.github.io/fractals/">Fractals</a>' +
+		'<a href="https://nichodon.github.io/programming/">Programming</a>' +
+		'<a href="https://nichodon.github.io/coming_soon/">Calculators</a>' +
+		'<a href="https://nichodon.github.io/coming_soon/">More Math</a>' +
+		'<a href="https://nichodon.github.io/coming_soon/">Worksheets</a>' +
+		'<a href="https://nichodon.github.io/about/">About</a>' +
+	'</div>';
 document.getElementsByClassName("footer")[0].innerHTML =
 	"<p>Copyright &copy; 2017 &ndash; All Rights Reserved &ndash; <a href=\"https://github.com/Nichodon\" target=\"_blank\">Nichodon</a> &ndash; <a href=\"#top\">Go to Top</a></p>";
 
@@ -26,12 +36,11 @@ for (var i = 0; i < linkz.length; i++) {
 
 window.onscroll = function() {
 	"use strict";
-	var top = document.getElementById("top");
 	var navbar = document.getElementsByClassName("topnav")[0];
-	if (window.scrollY > (top.offsetTop + top.offsetHeight)) {
-		navbar.classList.add('sticky');
+	if (window.scrollY > (navbar.offsetTop + navbar.offsetHeight)) {
+		document.getElementById('sticky').style.opacity = 1;
 	}
 	else {
-		navbar.classList.remove('sticky');
+		document.getElementById('sticky').style.opacity = 0;
 	}
 };
