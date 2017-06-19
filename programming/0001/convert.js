949152cf3bf67afcf9db2ea1d27a2801690247fe
Nichodon <brandonsyc.2222@yahoo.com>
2017-06-19 09:57:23 -0700

    fix converter

--- programming/0001/convert.js
+++ programming/0001/convert.js
@@ -1,12 +1,13 @@
 // JavaScript Document
 var units = [["gram", "grams", "g"], ["pound", "pounds", "lb"], ["kilogram", "kilograms", "kg"]];
 var values = [0.001, 2.20462262185, 1];
+var numbers = [-1, -1];
 
 function search(x)
 {
 	"use strict";
 	var search = document.getElementsByClassName("search")[x];
-	var unit = document.getElementsByClassName("unit")[x];
+	var unit = document.getElementsByClassName("unit")[x / 2];
 	var number = -1;
 	for (var i = 0; i < units.length; i++) {
 		var current = units[i];
@@ -19,13 +20,22 @@ function search(x)
 			break;
 		}
 	}
+	numbers[x / 2] = number;
 	if (number !== -1) {
 		unit.innerHTML = units[number][0].charAt(0).toUpperCase() + units[number][0].slice(1);
-		var input = document.getElementsByClassName("search")[2];
-		var output = document.getElementsByClassName("search")[3];
-		output.value = input.value;
 	}
 	else {
 		unit.innerHTML = "Input";
+	}
+	check();
+}
+
+function check()
+{
+	"use strict";
+	var input = document.getElementsByClassName("search")[1];
+	var output = document.getElementsByClassName("search")[3];
+	if (numbers[0] > -1 && numbers[1] > -1 && input.value !== "") {
+		output.value = input.value * values[numbers[0]] / values[numbers[1]];
 	}
 }
