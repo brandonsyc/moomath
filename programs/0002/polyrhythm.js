59f9ae561399c3082625e558e04ed53cec668365
anematode <timothy.herchen@gmail.com>
2017-06-20 23:09:50 -0700

    Polyrhythm updates

--- programs/0002/polyrhythm.js
+++ programs/0002/polyrhythm.js
@@ -1,51 +1,148 @@
 var beatNum = 0;
 var play = false;
 
-var beat1 = 3;
+var beat1 = 5;
 var beat2 = 3;
 
 var bpm = 120;
 
+var c=document.getElementById("beater");
+var ctx=c.getContext("2d");
+
+var backAndForth = false;
+
+var start;
+var nextAt;
+var beatsLater;
+
 function playSoundAsync(url) {
 	"use strict";
-    new Audio(url).play();
+    // document.getElementById(url).play();
+    new Audio("sounds/" + url + ".mp3").play();
+}
+
+function resetVars() {
+    start = null;
+    nextAt = null;
+    
+    oneFrameCircles = [];
+    queuedCircles = [];
+    
+    beatsLater = 1;
 }
 
 function togglePlayback() {
 	"use strict";
     play = !play;
     if (play) {
+        beatsLater = 1;
         beatNum = 0;
         document.getElementById("toggle").innerHTML = "Stop";
         playBeat();
+        start = new Date().getTime();
+        nextAt = start;
     }
 	else {
         document.getElementById("toggle").innerHTML = "Play";
+        drawBeats();
     }
 }
 
 function playBeat() {
 	"use strict";
+    
+    var drift = (new Date().getTime() - start) % (beatsLater*60000.0/Math.max(beat1,beat2)/bpm);
     if (beat1 === -1 || beat2 === -1) {
         play = false;
         document.getElementById("toggle").innerHTML = "Play";
         return;
     }
     if (beatNum % (beat1 * beat2) === 0) {
-        playSoundAsync("sounds/accent1.mp3");
+        playSoundAsync("accent1");
+        flyLine(0,false);
+        queueFillCircle(0, 0, beat1, 10, "red");
+        queueFillCircle(0, 1, beat2, 10, "red");
     }
 	else {
         if (beatNum % beat1 === 0) {
-            playSoundAsync("sounds/click1.mp3");
+            playSoundAsync("click1");
+            queueFillCircle((beatNum/beat2) % beat1, 1, beat1, 10, "green");
         }
-		else if (beatNum % beat2 === 0) {
-            playSoundAsync("sounds/click2.mp3");
+        if (beatNum % beat2 === 0) {
+            playSoundAsync("click2");
+            queueFillCircle((beatNum/beat1) % beat2, 0, beat2, 10, "green");
         }
     }
-    beatNum += 1;
+    for (i = beatNum+1; i <= beat1*beat2 + beatNum - (beatNum % (beat1*beat2)); i++) {
+        if (i % beat1 == 0 || i % beat2 == 0) {
+            beatsLater = i - beatNum;
+            break;
+        }
+    }
+    beatNum += beatsLater;
+    nextAt += beatsLater * 60000.0/Math.max(beat1,beat2)/bpm;
+    
+    /**beatNum += 1;
+    nextAt += 60000.0/Math.max(beat1,beat2)/bpm;**/
     if (play) {
         setTimeout(function(){
-                   playBeat();}, 60000.0/Math.max(beat1,beat2)/bpm);
+                   playBeat();}, nextAt - new Date().getTime());
+    }
+    return;
+}
+
+function drawCircle(a, beat, beats, rad) {
+    ctx.beginPath();
+    ctx.arc(1300/beats*a + 100,100*beat+50,rad,0,2*Math.PI);
+    ctx.stroke();
+}
+
+function drawFillCircle(a, beat, beats, rad, color) {
+    ctx.beginPath();
+    ctx.arc(1300/beats*a + 100,100*beat+50,rad,0,2*Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.stroke();
+}
+
+function initializeAudio() {
+    var sound = document.createElement('audio');
+    sound.setAttribute('src', "sounds/click1.mp3");
+    sound.setAttribute('id', 'click1');
+    document.body.appendChild(sound);
+    
+    sound = document.createElement('audio');
+    sound.setAttribute('src', "sounds/click2.mp3");
+    sound.setAttribute('id', 'click2');
+    document.body.appendChild(sound);
+    
+    sound = document.createElement('audio');
+    sound.setAttribute('src', "sounds/accent1.mp3");
+    sound.setAttribute('id', 'accent1');
+    document.body.appendChild(sound);
+}
+
+var queuedCircles = [];
+var oneFrameCircles = [];
+
+function queueFillCircle(a, beat, beats, rad, color) {
+    queuedCircles.push([a, beat, beats, rad, color]);
+    return;
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
+function drawBeats() {
+    clearCanvas();
+    
+    for (i = 0; i <= beat1; i++) {
+        drawCircle(i, 0, beat1, 10);
+    }
+    
+    for (i = 0; i <= beat2; i++) {
+        drawCircle(i, 1, beat2, 10);
     }
 }
 
@@ -60,9 +157,49 @@ function updateRhythm() {
         
         play = false;
         document.getElementById("toggle").innerHTML = "Play";
+        
+        drawBeats();
+        
         return;
     }
+    resetVars();
 }
+
+function flyLine(s,back) {
+    drawBeats();
+    var x = 100+1300/(60*Math.min(beat1,beat2)/bpm)*s;
+    if (x > 1400) {
+        if (backAndForth) {
+            flyLine(s,true);
+        }
+        return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(x,0);
+    ctx.lineTo(x,200);
+    ctx.stroke();
+    for (i = 0; i < queuedCircles.length; i++) {
+        circle = queuedCircles[i];
+        drawFillCircle(circle[0], circle[1], circle[2], circle[3], circle[4]);
+    }
+    for (i = 0; i < oneFrameCircles.length; i++) {
+        circle = oneFrameCircles[i];
+        drawFillCircle(circle[0], circle[1], circle[2], circle[3], circle[4]);
+    }
+    oneFrameCircles = [];
+    oneFrameCircles = queuedCircles;
+    queuedCircles = [];
+    if (play) {
+        if (back) {
+            setTimeout(function(){
+                   flyLine(s-1/60.0);}, 1000.0/60.0);
+        } else {
+            setTimeout(function(){
+                       flyLine(s+1/60.0);}, 1000.0/60.0);
+        }
+    }
+}
+
 
 function updateBPM() {
     "use strict";
@@ -70,4 +207,16 @@ function updateBPM() {
     if (1 < givenBPM && givenBPM < 500) {
         bpm = givenBPM;
     }
+    resetVars();
 }
+
+updateRhythm();
+updateBPM();
+// initializeAudio();
+
+
+
+
+
+
+
