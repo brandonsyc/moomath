7afb40f24b00cfd7227241a61618bc2cfce51643
Nichodon <brandonsychung@gmail.com>
2017-06-29 20:40:15 -0700

    add two bodies

--- programs/0005/planet.js
+++ programs/0005/planet.js
@@ -1,15 +1,30 @@
 // JavaScript Document
 var canvas = document.getElementsByTagName("canvas")[0];
 var ctx = canvas.getContext("2d");
-var x = 0;
+var bodies = [[300.0, 200.0, 0, 0.0, 1000], [200.0, 250.0, 0.5, 2.0, 1]];
 
 function bulb() {
 	"use strict";
-	++x;
    	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.beginPath();
-	ctx.arc(100, x, 100, 0, 2 * Math.PI);
-	ctx.stroke();
+	for (var i = 0; i < bodies.length; i++) {
+		var body = bodies[i];
+		ctx.beginPath();
+		ctx.arc(body[0], body[1], 10, 0, 2 * Math.PI);
+		ctx.stroke();
+		body[0] += body[2];
+		body[1] += body[3];
+		for (var j = 0; j < bodies.length; j++) {
+			if (i !== j) {
+				var other = bodies[j];
+				var distance = Math.hypot(other[0] - body[0], other[1] - body[1]);
+				var magnitude = other[4] / (distance * distance);
+				var ax = magnitude * (other[0] - body[0]) / distance;
+				var ay = magnitude * (other[1] - body[1]) / distance;
+				body[2] += ax;
+				body[3] += ay;
+			}
+		}
+	}
 	setTimeout(function() {
 		bulb(); 
 	}, 1000.0/60.0);
