9c4cf3ea81950017955a5fbf1dbe8750c84bedca
Nichodon <brandonsychung@gmail.com>
2017-06-30 08:46:47 -0700

    add more functionality to planets

--- programs/0005/planet.js
+++ programs/0005/planet.js
@@ -2,15 +2,18 @@
 var canvas = document.getElementsByTagName("canvas")[0];
 var ctx = canvas.getContext("2d");
 var bodies = [[300.0, 200.0, 0, 0.0, 1000], [200.0, 250.0, 0.5, 2.0, 1]];
+var mouse;
+var colors = ["#096", "#08b", "#c03", "#fd0"];
 
 function bulb() {
 	"use strict";
    	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (var i = 0; i < bodies.length; i++) {
 		var body = bodies[i];
+		ctx.fillStyle = colors[0];
 		ctx.beginPath();
 		ctx.arc(body[0], body[1], 10, 0, 2 * Math.PI);
-		ctx.stroke();
+		ctx.fill();
 		body[0] += body[2];
 		body[1] += body[3];
 		for (var j = 0; j < bodies.length; j++) {
@@ -25,8 +28,43 @@ function bulb() {
 			}
 		}
 	}
+	
+	if (document.getElementById("body").checked) {
+		if (mouse !== undefined) {
+			ctx.fillStyle = "rgba(51, 51, 51, 0.1)";
+			ctx.beginPath();
+			ctx.arc(mouse.x, mouse.y, 10, 0, 2 * Math.PI);
+			ctx.fill();
+		}
+	}
 	setTimeout(function() {
 		bulb(); 
 	}, 1000.0/60.0);
 }
 bulb();
+
+function clicked() {
+	"use strict";
+	if (document.getElementById("body").checked) {
+		bodies.push([mouse.x, mouse.y, -1.5, 0, 100]);
+	}
+}
+
+function getMousePos(canvas, evt) {
+	"use strict";
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+	"use strict";
+	mouse = getMousePos(canvas, evt);
+}, false);
+
+canvas.addEventListener('mouseout', function(evt) {
+	"use strict";
+	mouse = undefined;
+}, false);
