51d6520b144fb895fdf4a4295f709574486f48f7
Nichodon <brandonsychung@gmail.com>
2017-07-13 19:28:53 -0700

    add unit visualizations

--- programs/0001/convert.js
+++ programs/0001/convert.js
@@ -48,7 +48,7 @@ function search(x)
 	"use strict";
 	var type = document.querySelector('input[name = "type"]:checked').value;
 	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", type + ".txt", false);
+	rawFile.open("GET", "https://nichodon.github.io/programs/0001/" + type + ".txt", false);
 	rawFile.onreadystatechange = function ()
 	{
 		if(rawFile.readyState === 4)
@@ -70,12 +70,11 @@ function search(x)
 
 	var search = document.getElementsByClassName("search")[x];
 	var unit = document.getElementsByClassName("unit")[x / 2];
+	var square = document.getElementsByClassName("square")[x / 2];
 	var number = -1;
 
 	var lowestEditDistance = 15;
-
 	var foundPerfect = false;
-
 	var editDistance = null;
 
 	var searchvalue = search.value.toLowerCase();
@@ -101,9 +100,11 @@ function search(x)
 	numbers[x / 2] = number;
 	if (number !== -1 && search.value !== "") {
 		unit.innerHTML = units[number][0].charAt(0).toUpperCase() + units[number][0].slice(1) + " (" + units[number][1].trim() + ")";
+		square.className = "square " + ["green", "blue"][x / 2];
 	}
 	else {
 		unit.innerHTML = "Input";
+		square.className = "square";
 	}
 	check();
 }
@@ -113,8 +114,23 @@ function check()
 	"use strict";
 	var input = document.getElementsByClassName("search")[1];
 	var output = document.getElementsByClassName("search")[3];
-	if (numbers[0] > -1 && numbers[1] > -1 && input.value !== "") {
-		output.value = input.value * values[numbers[0]] / values[numbers[1]];
+	var squares = document.getElementsByClassName("square");
+	if (numbers[0] > -1 && numbers[1] > -1) {
+		if (input.value !== "") {
+			output.value = input.value * values[numbers[0]] / values[numbers[1]];
+		}
+		if (values[numbers[0]] - values[numbers[1]] > 0) {
+			squares[0].style.width = "100%";
+			squares[0].style.paddingTop = "100%";
+			squares[1].style.width = values[numbers[1]] * 100 / values[numbers[0]] + "%";
+			squares[1].style.paddingTop = values[numbers[1]] * 100 / values[numbers[0]] + "%";
+		}
+		else {
+			squares[0].style.width = values[numbers[0]] * 100 / values[numbers[1]] + "%";
+			squares[0].style.paddingTop = values[numbers[0]] * 100 / values[numbers[1]] + "%";
+			squares[1].style.width = "100%";
+			squares[1].style.paddingTop = "100%";
+		}
 	}
 	else {
 		output.value = "";
