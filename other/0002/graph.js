50b861eebe2ff91554dc1e5db6322a155475fa89
Nichodon <brandonsychung@gmail.com>
2017-08-16 18:14:47 -0700

    add controls

--- other/0002/graph.js
+++ other/0002/graph.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("c");
 var ctx = canvas.getContext("2d");
 
+var user = document.getElementById("user");
+var xes = document.getElementById("xes");
+
 function run() {
 	"use strict";
 	ctx.fillStyle = "#fff";
@@ -13,5 +16,24 @@ setInterval(function() {
 }, 1000 / 60);
 
 function drawGrid() {
+	"use strict";
+	ctx.fillStyle = "#eee";
 	
+}
+
+function addEquation() {
+	"use strict";
+	var input = document.createElement("INPUT");
+	var li1 = document.createElement("LI");
+	li1.classList.add("active");
+	li1.appendChild(input);
+	var children = user.children;
+	user.insertBefore(li1, children[children.length - 1]);
+	input.focus();
+	
+	var button = document.createElement("BUTTON");
+	button.innerHTML = "&#128473;";
+	var li2 = document.createElement("LI");
+	li2.appendChild(button);
+	xes.appendChild(li2);
 }
