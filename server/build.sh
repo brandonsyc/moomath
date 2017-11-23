213a2344911206a7bea3f9ab98e0eff9e17a9c38
anematode <timothy.herchen@gmail.com>
2017-11-23 00:59:09 -0800

    fix build once and for all (?)

--- server/build.sh
+++ server/build.sh
@@ -3,9 +3,10 @@
 # Compile asteroidresponder
 g++ -o ../programs/0006/server/astSearch ../programs/0006/server/asteroidresponder.cpp -O3 -std=c++11
 
-$formatted_time date
-
-echo "<html><body style='white-space: pre-wrap'>Build completed at " + $formatted_time + ".</body></html>" > build.html
+echo "" > build.html
+echo "<html><body style='white-space: pre-wrap'>Build completed at " >> build.html
+echo $(date) >> build.html
+echo "</body></html>" >> build.html
 
 # Restart Apache
 apachectl restart
