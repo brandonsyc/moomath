2523d1a1d066429f4043df5d793e2d7b209c143d
anematode <timothy.herchen@gmail.com>
2017-11-30 18:47:00 -0800

    add 404 to converter

--- server/build.sh
+++ server/build.sh
@@ -4,7 +4,7 @@
 g++ -o ../programs/0006/server/astSearch ../programs/0006/server/asteroidresponder.cpp -O3 -std=c++11
 
 # Convert nichodon.github.io links into moomath links (hopefully temporary)
-python moomath_convert.py paginate.js frame.js sitemap.xml search.js other/0001/index.html programs/0001/index.html
+python moomath_convert.py paginate.js frame.js sitemap.xml search.js other/0001/index.html programs/0001/index.html 404.html
 
 # Convert 404.html
 python trim404.py
