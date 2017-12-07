ca5720d8138f502652a17095b9c2ddcffd8f9f20
anematode <timothy.herchen@gmail.com>
2017-12-06 21:06:33 -0800

    updated some server stuff

--- server/build.sh
+++ server/build.sh
@@ -6,8 +6,5 @@ g++ -o ../programs/0006/server/astSearch ../programs/0006/server/asteroidrespond
 # Convert nichodon.github.io links into moomath links (hopefully temporary)
 python moomath_convert.py paginate.js frame.js sitemap.xml search.js other/0001/index.html programs/0001/index.html 404.html images/moo.js
 
-# Convert 404.html
-python trim404.py
-
 # Restart Apache
 apachectl restart
