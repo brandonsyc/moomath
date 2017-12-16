ca08ed48a4d2899d10a07ac43a50288c9b4b9b80
anematode <timothy.herchen@gmail.com>
2017-12-15 20:04:37 -0800

    fix 0006... ?

--- server/build.sh
+++ server/build.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-# Kill any old asteroidresponder instances
-pkill -f astSearch-qi4PFgOgvR
-
-# Compile asteroidresponder
-g++ -o ../programs/0006/server/astSearch-qi4PFgOgvR ../programs/0006/server/asteroidresponder.cpp -Ofast -march=native -std=c++11
+# Setup asteroidresponder stuffs
+python ../programs/0006/server/setup.py
 
 # Convert nichodon.github.io links into moomath links (hopefully temporary)
 python moomath_convert.py paginate.js frame.js sitemap.xml search.js other/0001/index.html programs/0001/index.html 404.html images/moo.js
