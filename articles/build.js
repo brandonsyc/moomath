8698a544fed7e648e03d6fd7b370f13b50e60dab
Nichodon <brandonsychung@gmail.com>
2018-01-08 09:22:25 -0800

    add figures and code

--- articles/build.js
+++ articles/build.js
@@ -13,11 +13,20 @@ rawFile.onreadystatechange = function () {
 			document.body.appendChild(article);
 			
 			var out = "<p>";
+			var setting = "";
 			
 			for (var i = 0; i < array.length; i++) {
-				if (array[i].includes("::")) {
+				if (array[i].includes("::fig::")) {
 					var pars = array[i].split("::");
-					out += "<figure>\n\t<img src=\"" + pars[1] + "\" alt=\"" + pars[2] + "\">\n\t<figcaption>" + pars[3] + "</figcaption>\n</figure>";
+					out += "<figure>\n\t<img src=\"" + pars[2] + "\" alt=\"" + pars[3] + "\">\n\t<figcaption>" + pars[4] + "</figcaption>\n</figure>\n";
+				} else if (array[i].includes("::code::")) {
+					setting = "code";
+					out += "<pre class=\"prettyprint\">\n";
+				} else if (array[i] === "::") {
+					if (setting === "code") {
+						out += "</pre>";
+					}
+					setting = "";
 				} else if (array[i] === "") {
 					if (!array[i - 1].includes("::")) {
 						out += "</p>\n\n";
