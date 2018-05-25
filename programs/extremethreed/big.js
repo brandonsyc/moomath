7c042b42051162a2e200de353c10bdd3ec8e25c9
Nichodon <brandonsychung@gmail.com>
2018-05-24 21:08:29 -0700

    add first extreme 3d test

---
+++ programs/extremethreed/big.js
@@ -0,0 +1,87 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+camera.position.z = 100;
+
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var light = new THREE.PointLight( 0xffffff, 1, 0 );
+light.position.set( 0, 0, 100 );
+scene.add( light );
+
+class Body {
+    constructor(px, py, pz, vx, vy, vz, mass, obj) {
+        this.px = px;
+        this.py = py;
+        this.pz = pz;
+        this.vx = vx;
+        this.vy = vy;
+        this.vz = vz;
+        this.mass = mass;
+        this.obj = obj;
+    }
+}
+
+function distance(b1, b2) {
+    "use strict";
+    var dx = b2.px - b1.px;
+    var dy = b2.py - b1.py;
+    var dz = b2.pz - b1.pz;
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+function calc(b1, b2) {
+    "use strict";
+    var dist = distance(b1, b2);
+    var mag = b2.mass / (dist * dist);
+    b1.vx += mag * (b2.px - b1.px) / dist;
+    b1.vy += mag * (b2.py - b1.py) / dist;
+    b1.vz += mag * (b2.pz - b1.pz) / dist;
+}
+
+var bodies = [];
+
+var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+var material = new THREE.MeshPhongMaterial( {color: 0x00ffff} );
+var sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+bodies.push(new Body(0, 0, 0, 0.01, 0, -0.015, 10, sphere));
+
+var geometry = new THREE.SphereGeometry( 2, 32, 32 );
+var material = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
+var sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+bodies.push(new Body(0, 60, 0, -0.1, 0, 0.15, 1, sphere));
+
+function animate() {
+    "use strict";
+	requestAnimationFrame( animate );
+    
+    for (var i = 0; i < bodies.length; i++) {
+        var body = bodies[i];
+        body.obj.position.x = body.px;
+        body.obj.position.y = body.py;
+        body.obj.position.z = body.pz;
+    
+        for (var j = 0; j < bodies.length; j++) {
+            if (j != i) {
+                calc(body, bodies[j]);
+            }
+        }
+    }
+    
+    
+    for (var i = 0; i < bodies.length; i++) {
+        var body = bodies[i];
+        body.px += body.vx;
+        body.py += body.vy;
+        body.pz += body.vz;
+    }
+    
+	renderer.render( scene, camera );
+}
+animate();
