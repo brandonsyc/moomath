3d4210a5bf61e6a0b34aadfe01cbb9c3fbe660a9
Nichodon <brandonsychung@gmail.com>
2018-06-16 07:03:34 -0700

    rewrite isotope finder

--- files/chem/index.js
+++ files/chem/index.js
@@ -1,8 +1,3 @@
-let query = 'Q556'
-
-const fs = require('fs')
-
-let thing
 let props = {
     'P816': 'Daughter',
     'P1086': 'Protons',
@@ -14,28 +9,56 @@ let props = {
     'P2374': 'Abundance'
 }
 
-fs.readFile("props.json", function(e, data) {
-  thing = JSON.parse(data)
-})
 let all = ['P1086', 'P1148', 'P2067']
+let query = 'Q556'
+let data = {}
 
 const request = require('request')
+const fs = require('fs')
 
-request('https://www.wikidata.org/w/api.php?action=wbgetentities&ids=' + query + '&format=json', function(e, response) {
-  let json = JSON.parse(response.body)
-  let keys = Object.keys(thing)
-  for (let i = 0; i < keys.length; i++) {
-    let prop = json['entities'][query]['claims'][keys[i]]
-    if (prop === undefined) {
-      console.log('undef')
-    } else {
-      console.log(json['entities'][query]['claims'][keys[i]][0]['mainsnak']['datavalue']['value']['amount'])
+request('https://query.wikidata.org/sparql?query=SELECT%20%3Fa%20WHERE%20%7B%20%3Fa%20wdt%3AP279%20wd%3A' + query + '.%20%7D&format=json', function(error, response) {
+    let json = JSON.parse(response.body)
+    for (let i = 0; i < json['results']['bindings'].length; i++) {
+        
+        let url = json['results']['bindings'][i]['a']['value']
+        request(url, function(error, response) {
+            let nested = JSON.parse(response.body)
+            let wiki = nested['entities'][url.split('/')[4]]['claims']
+            let keys = Object.keys(props)
+            let inside = {}
+            let valid = true
+            for (let j = 0; j < keys.length; j++) {
+                if (wiki[keys[j]] === undefined) {
+                    if (all.indexOf(keys[j]) >= 0) {
+                        valid = false
+                        break
+                    }
+                } else {
+                    if (wiki[keys[j]][0]['mainsnak']['datavalue'] === undefined) {
+                        inside[props[keys[j]]] = ''
+                    } else {
+                        let value = wiki[keys[j]][0]['mainsnak']['datavalue']['value']
+                        inside[props[keys[j]]] = value['id'] || value['amount']
+                        if (keys[j] == 'P2114') {
+                            inside['HLU'] = value['unit']
+                            let u2 = value['unit']
+                            request(u2, function(e2, r2) {
+                                let n2 = JSON.parse(r2.body)
+                            })
+                        }
+                    }
+                }
+            }
+            if (valid) {
+                edit(nested['entities'][url.split('/')[4]]['labels']['en']['value'], inside)
+            }
+        })
     }
-  }
 })
 
+function edit(k, v) {
+    data[k] = v
 
-request('https://query.wikidata.org/sparql?query=SELECT%20%3Fsub%20WHERE%20%7B%0A%20%20%3Fsub%20wdt%3AP279%20wd%3A' + query + '.%0A%7D&format=json', function(e, response) {
-  let json = JSON.parse(response.body)
-  console.log(json['results']['bindings'][0]['sub']['value'])
-})
+    let jjjj = JSON.parse(JSON.stringify(data))
+    fs.writeFile("test.json", JSON.stringify(jjjj, null, '\t'), function(err) {})
+}
