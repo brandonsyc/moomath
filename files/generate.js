4c26fd111d844d0e17298c472a11cf9265e8829e
Nichodon <brandonsychung@gmail.com>
2018-06-16 15:47:58 -0700

    adjust colors

--- files/generate.js
+++ files/generate.js
@@ -8,6 +8,8 @@ const gaps = [Array.from(new Array(17), (x,i) => i + 1),
 
 console.log(gaps);
 
+const c = [0xe57373, 0xffb74d, 0xfff176, 0xdce775, 0x81c784, 0x4dd0e1, 0x64b5f6, 0x7986cb, 0xba68c8, 0x90a4ae];
+
 const svgns = "http://www.w3.org/2000/svg";
 let n = 0;
 for (let i = 0; i < 10; i++) {
@@ -20,25 +22,25 @@ for (let i = 0; i < 10; i++) {
             rect.setAttributeNS(null, 'width', 100);
             let color = 0x000000;
             if (i === 6 && j > 8 && j !== 12) {
-                color = 0x607d8b;
+                color = c[9];
             } else if (j === 0 && i > 0) {
-                color = 0xf44336;
+                color = c[0];
             } else if (j === 1) {
-                color = 0xff9800;
+                color = c[1];
             } else if (i === 8 || (j === 2 && i === 5)) {
-                color = 0xffeb3b;
+                color = c[2];
             } else if (i === 9 || (j === 2 && i === 6)) {
-                color = 0xcddc39;
+                color = c[3];
             } else if (j < 12 && j > 1) {
-                color = 0x4caf50;
+                color = c[4];
             } else if (j === 18) {
-                color = 0x9c27b0;
+                color = c[8];
             } else if (j - i > 12 || (j === 0 && i === 0)) {
-                color = 0x3f51b5;
+                color = c[7];
             } else if (j - i > 11 || (j === 14 && i === 3) || (j === 15 && i === 4)) {
-                color = 0x2196f3;
+                color = c[6];
             } else {
-                color = 0x00bcd4;
+                color = c[5];
             }
             rect.setAttributeNS(null, 'fill', '#' + color.toString(16).padStart(6, '0'));
             document.getElementById('table').appendChild(rect);
@@ -46,6 +48,7 @@ for (let i = 0; i < 10; i++) {
             let text = document.createElementNS(svgns, 'text');
             text.setAttributeNS(null, 'x', j * 100 + 50);
             text.setAttributeNS(null, 'y', i * 100 + 50);
+            text.setAttributeNS(null, 'opacity', 0.9);
             text.setAttributeNS(null, 'alignment-baseline', 'middle');
             text.setAttributeNS(null, 'text-anchor', 'middle');
             text.textContent = atoms[n];
