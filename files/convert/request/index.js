ece574e645a85ab5dd7bb691e2b9171519c227be
Nichodon <brandonsychung@gmail.com>
2018-09-05 17:56:13 -0700

    update aliases
    for the exciting moonit things

--- files/convert/request/index.js
+++ files/convert/request/index.js
@@ -1,17 +1,21 @@
-let request = require('request')
+const request = require('request')
+const fs = require('fs')
 
 let data = {};
 let aliases = {};
 
 function format(x) {
-    let out = x.split('&amp;').join('')
-    out = out.split('nbsp;').join(' ')
-    out = out.split('lt;').join('<')
-    out = out.split('gt;').join('>')
+    let out = x
+    out = out.split('&amp;nbsp;').join('\u00A0')
+    out = out.split('&amp;thinsp;').join('\u2009')
+    out = out.split('&amp;#8209;').join('\u2011')
+    out = out.split('&lt;').join('<')
+    out = out.split('&gt;').join('>')
     return out
 }
 
 request('https://en.wikipedia.org/wiki/Module:Convert/data', function(error, response) {
+    console.log('Got response.')
     let body = response.body.split('---------------------------------------------------------------------------')[2]
     body = body.split('\t').join('')
     body = body.split('&quot;').join('"')
@@ -19,12 +23,19 @@ request('https://en.wikipedia.org/wiki/Module:Convert/data', function(error, res
     units.forEach(element => {
         let lines = element.split('\n')
         let initial = lines[0].split('"')[1]
+        if (!initial) {
+            return
+        }
         let values = {}
         lines.forEach(line => {
             if (line.includes('name1 ')) {
                 values['name1'] = line.split('"')[1]
             } else if (line.includes('name1_us ')) {
                 values['name1_us'] = line.split('"')[1]
+            }  else if (line.includes('name2 ')) {
+                values['name2'] = line.split('"')[1]
+            }  else if (line.includes('name2_us ')) {
+                values['name2_us'] = line.split('"')[1]
             } else if (line.includes('scale ')) {
                 values['scale'] = line.split('= ')[1].split(',')[0]
             } else if (line.includes('target ')) {
@@ -46,12 +57,27 @@ request('https://en.wikipedia.org/wiki/Module:Convert/data', function(error, res
             }
             data[values['utype']][format(name)] = {
                 'scale': values['scale'],
-                'symbol': format(values['symbol'] || initial)
+                'symbol': format(values['symbol'] || initial),
+                'code': format(initial)
             }
             if (initial !== name) {
                 aliases[initial] = format(name)
             }
+            if (values['name1_us'] && values['name1_us'] !== name) {
+                aliases[format(values['name1_us'])] = format(name)
+            }
+            if (values['name2']) {
+                aliases[format(values['name2'])] = format(name)
+            }
+            if (values['name2_us']) {
+                aliases[format(values['name2_us'])] = format(name)
+            }
         }
     })
-    console.log(data)
+    //console.log(data)
+    fs.writeFile('data.json', JSON.stringify({
+        'units': data,
+        'aliases': aliases
+    }, null, '\t'), function(err) {})
+    console.log('Saved data.')
 })
